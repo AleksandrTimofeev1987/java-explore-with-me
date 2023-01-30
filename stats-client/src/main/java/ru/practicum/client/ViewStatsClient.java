9a86b819ca50f592b95a22d82e6fc0543afeeb40
Sasha <timofeev.aleksandr.1987@yandex.ru>
2023-01-30 22:18:04 +0300

    1st review fix: fix: use environment variable for stats-server.url in ewm-service and stats-client fix: put maps versioned mapstruct dependencies in parent pom fix: make ViewStats class variables private

--- stats-client/src/main/java/ru/practicum/client/ViewStatsClient.java
+++ stats-client/src/main/java/ru/practicum/client/ViewStatsClient.java
@@ -17,7 +17,7 @@ public class ViewStatsClient extends BaseClient {
     private static final String API_PREFIX = "/stats";
 
     @Autowired
-    public ViewStatsClient(@Value("http://localhost:9090") String serverUrl, RestTemplateBuilder builder) {
+    public ViewStatsClient(@Value("${stats-server.url}") String serverUrl, RestTemplateBuilder builder) {
         super(builder
                 .uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl + API_PREFIX))
                 .requestFactory(HttpComponentsClientHttpRequestFactory::new)
