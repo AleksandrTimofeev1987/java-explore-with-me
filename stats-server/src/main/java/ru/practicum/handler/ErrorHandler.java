ed78c9b854df7cdce8b4a4ffd33acb28ef0fdfbe
Sasha <timofeev.aleksandr.1987@yandex.ru>
2023-02-08 10:51:53 +0300

    2nd stage: refactor: bring exception handler in stats-server in line with ewm-service handler

--- stats-server/src/main/java/ru/practicum/handler/ErrorHandler.java
+++ stats-server/src/main/java/ru/practicum/handler/ErrorHandler.java
@@ -2,19 +2,26 @@ package ru.practicum.handler;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.practicum.model.ApiError;
 import ru.practicum.model.BadRequestException;
-import ru.practicum.model.ErrorResponse;
+
+import java.time.LocalDateTime;
 
 @RestControllerAdvice("ru.practicum")
 @Slf4j
 public class ErrorHandler {
     @ExceptionHandler
     @ResponseStatus(value = HttpStatus.BAD_REQUEST)
-    public ErrorResponse handleBadRequest(final BadRequestException e) {
+    public ResponseEntity<Object> handleBadRequest(final BadRequestException e) {
         log.warn(e.getMessage());
-        return new ErrorResponse(e.getMessage());
+        return buildResponseEntity(new ApiError(HttpStatus.BAD_REQUEST, "Bad request", e.getMessage(), LocalDateTime.now()));
+    }
+
+    private ResponseEntity<Object> buildResponseEntity(ApiError apiError) {
+        return new ResponseEntity<>(apiError, apiError.getStatus());
     }
 }
